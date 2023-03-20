06a92e61ee4d5b6160474127c0a0bbc86c328dbf
Laminethrdream <oliver.laminethiam@gmail.com>
2023-03-20 19:34:48 +0100

    b

---
+++ content/canxaContent.js
@@ -0,0 +1,181 @@
+export const navbarContent = {
+  navLinks: {
+    home: "Home",
+    überUns: "Über uns",
+    unsereServices: "Unser Angebot",
+    prozess: "Ablauf",
+    bewertungen: " Kunden Bewertungen",
+    faq: "FAQ",
+    kontakt: "Kontakt",
+  },
+  buttonText: "Kontaktiere uns",
+};
+
+export const heroContent = {
+  title: "Webseiten Spezialisten",
+  description: "",
+  buttonText: "Erfahre mehr",
+};
+
+export const aboutUsContent = {
+  title: "Über uns",
+  description: `Von Webdesign bis Suchmaschinenoptimierung, 
+                wir helfen dir dabei deine Konkurrenz in den Schatten zu stellen.
+                Hier werden deine Gedanken und Fantasien in Webseiten umgewandelt.
+                Wir sind eine Gruppe aus Webdesign Spezialisten geschaffen um deinem Unternehmen die Tür in 
+                die Online Welt zu öffnen.
+                Wenn du deinen Traffic, deinen Umsatz und deine Sichtbarkeit steigern möchtest, kannst du gerne einen kostenlosen Termin 
+                über unser Kontaktformular erstellen.`,
+  checkBox1: "Top Coder",
+  checkBox2: "Top Preise",
+  checkBox3: "Top Beratung",
+  checkBox4: "Top Webseite",
+  buttonText: "Kontaktiere uns",
+};
+
+export const servicesContent = {
+  title: "Services",
+  service1: {
+    packageName: "Paket S",
+    price: "€ 199,-",
+    description: `Benötigst du eine informative Homepage als Addition zu deiner 
+                  Facebook Seite oder Google Business Card, dann ist das Paket S, dass richtige für dich.`,
+    listElement1: "1 Inhaltsseite",
+    listElement2: "5 Bilder, 1 Video",
+    listElement3: "Responsives Design kannst du das als Link darstellen",
+    listElement4: "Weitere Funktionen auf Anfrage",
+    buttonText: "Kontaktiere Uns",
+  },
+  service2: {
+    packageName: "Paket M",
+    price: "€ 249,-",
+    description: `Möchtest du deine Kunden informieren und ihnen 
+                  deine Dienstleistungen/ dein Produkt optimal näher bringen, ist dieses Paket das richtige.`,
+    listElement1: "bis zu 2 Inhaltsseite",
+    listElement2: "Kontaktformular, Suchfunktion",
+    listElement3: "Responsives Design kannst du das als Link darstellen",
+    listElement4: "Webseiten hosting",
+    buttonText: "Kontaktiere Uns",
+  },
+  service3: {
+    packageName: "Paket L",
+    price: "€ 449,-",
+    description: `Suchst du eine Möglichkeit dein Traffic zu erhöhen,
+                  die perfekte Webseiten zu besitzen und deine Webseite in Zukunft
+                   selber neu zu gestalten, dann wähle dieses Paket.`,
+    listElement1: "1 Inhaltsseite",
+    listElement2: "Kontaktformular, Suchfunktion",
+    listElement3: "Content Management",
+    listElement4: "Screen Anpassung",
+    buttonText: "Kontaktiere Uns",
+  },
+};
+
+export const processContent = {
+  title: "Prozess",
+  process1: {
+    header: "1. Erstgespräch & Beratung",
+    description: `Wir erfassen in einem Telefonat oder in einem persönlichen Gespräch deine Wünsche, Fragen und Ziele und beraten dich aus dem Blickwinkel eines Webseiten Spezialisten.`,
+  },
+  process2: {
+    header: "2. Angebotserstellung",
+    description: `Durch unsere Angebotspakete kannst du selbst deine Anforderungen definieren und uns im persönlichen Gespräch mitteilen`,
+  },
+  process3: {
+    header: "3. Webdesign Umsetzung",
+    description: `Wir designen deine Webseite, nach den aktuellsten Webdesign-Trends, genauso wie du es dir vorstellst.
+                 Möchtest du wissen wie weit deine Webseite im Entstehungsprozess ist kann du uns jederzeit erreichen und 
+                 den aktuellen Stand abfragen. So hast du die Möglichkeit bei Mängeln direkt enzuschreiten(Event. hier mit dem Wort transparent arbeiten)`,
+  },
+  process4: {
+    header: "4. Optimierung für Suchmaschinen",
+    description: `Mithilfe unsere Expertise in Suchmaschinenoptimierung und der Unterstützung modernster
+                   SEO-Tools erschaffen deine Webseite. Durch genaue Plazierung sogenannter Keywords wird
+                    es möglich deine Webseite an der Spitze der Suchergebnisse anzeigen zu lassen.`,
+  },
+  process5: {
+    header: "5. Abnahme und Veröffentlichung",
+    description: `Nachdem du uns dein GO gegeben hast werden wir deine Website veröffentlichen und über unseren Server hosten. 
+                  Je nachdem bieten wir eine Schulung für unser eigenes Content Management Interface an,
+                   um anschließende Veränderungen an der Webseiten für dich möglich zu machen.`,
+  },
+  process6: {
+    header: "6. Pflege, Updates & Reporting",
+    description: `Wir sorgen in regelmäßigen Abständen für Updates und pflegen deine Seiteninhalte. Auf Wunsch 
+                  führen wir eine erneute SEO durch um die Webseite auf dem neusten Stand zu halten.`,
+  },
+};
+
+export const reviewsContent = {
+  title: "Das sagen unsere Kunden",
+  titleDescription1:
+    " Was ist das Geheimnis unserer 100%igen Kundenzufriedenheit?",
+  titleDescription2:
+    "Ganz einfach: Wir versprechen nur, was wir auch einhalten können!",
+  card1: {
+    name: "Muhammed Yildiz",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque unde alias earum officiis sapiente! Non sapiente reiciendis officiis nulla.`,
+  },
+  card2: {
+    name: "Muhammed Yildiz",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque unde alias earum officiis sapiente! Non sapiente reiciendis officiis nulla.`,
+  },
+  card3: {
+    name: "Muhammed Yildiz",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque unde alias earum officiis sapiente! Non sapiente reiciendis officiis nulla.`,
+  },
+  card4: {
+    name: "Muhammed Yildiz",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque unde alias earum officiis sapiente! Non sapiente reiciendis officiis nulla.`,
+  },
+  card5: {
+    name: "Muhammed Yildiz",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque unde alias earum officiis sapiente! Non sapiente reiciendis officiis nulla.`,
+  },
+  card6: {
+    name: "Muhammed Yildiz",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque unde alias earum officiis sapiente! Non sapiente reiciendis officiis nulla.`,
+  },
+  question: "Hast du Fragen oder Anliegen?",
+  buttonText: "Kontaktiere uns",
+};
+
+export const faqContent = {
+  title: "FAQ",
+  faqs: [
+    {
+      question: "Kann ich meine Webseite auch selber erstellen?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui mollitia reiciendis vitae quasi",
+    },
+    {
+      question: "Wie kommt der Preis zustande?",
+      answer:
+        "Wir haben uns für diese Preise entschieden, weil wir auch kleineren Unternehmen die Tür zu einer qualitativ hochwertigen Webseite öffnen wollen",
+    },
+    {
+      question: "Was ist SEO?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui mollitia reiciendis vitae quasi",
+    },
+    {
+      question: "Warum solltest du uns wählen?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui mollitia reiciendis vitae quasi",
+    },
+  ],
+};
+
+export const kontaktContent = {
+  title: "Kontakt",
+  description: `Hier kannst du in direkten Kontakt mit unseren, am Projekt arbeitenden, 
+                Webdesignern treten, Um dein Anliegen zu deiner Zufriedenheit zu klären.`,
+  buttonText: "Senden",
+  kontaktBox: {
+    header: "Kontakt Informationen",
+    email: "Webseiten@spezialisten.com",
+    number: "01590 84234715",
+    adress: "51103 Köln, Graf-Straße 33",
+    times: "Mo - Fr: 09:00 Uhr - 18:00 Uhr",
+  },
+};
